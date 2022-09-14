01e558d0c8448402743ef2b04391f060709999c5
Alphalance247 <al.moshood017@gmail.com>
2022-09-14 22:20:54 +0100

    DICE CHALLENGE

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var randomNumber1 = Math.round(Math.random()*6);
+
 var randomDice = "dice" +randomNumber1+ ".png"
+
 var randomImageSource = "images/" +randomDice;
+
 document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);
 
 
@@ -8,8 +11,12 @@ document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);
 // sceond randon number
 
 var randomNumber2 = Math.floor(Math.random()*6)+1;
+
 var randomImg2 = "images/dice" +randomNumber2+ ".png";
+
 document.querySelectorAll("img")[1].setAttribute("src", randomImg2);
+
+// else and if statements
 
 if(randomImageSource > randomImg2){
     document.querySelector("h1").innerHTML = "player 1 wins";
